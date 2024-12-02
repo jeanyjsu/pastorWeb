7f9107c3996633f2ce44b305f7c79e97cf5d3f59
jeanyjsu <jeanyjsu@hotmail.com>
2024-12-01 16:46:47 -0800

    change foot

--- app/components/footer/footers.js
+++ app/components/footer/footers.js
@@ -47,7 +47,7 @@ const Footers = ({ setIsLoggedIn }) => {
     //Once submitted to match with database
     const handleSubmit = async () => {
         try {
-            const response = await fetch('http://localhost:5000/api/admin/login', {
+            const response = await fetch('https://pastor-web-backend.vercel.app/api/admin/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
